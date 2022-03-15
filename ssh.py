6bac1c6c626b090f9cab878b3f372dcccf0289e7
Xavier Pisco <x.pisco@gmail.com>
2022-03-15 09:31:40 +0000

    Automatic connection to VMs

---
+++ ssh.py
@@ -0,0 +1,13 @@
+import os
+from sys import argv
+
+if len(argv) == 1:
+    print("Usage: python ssh.py [A|B|C]")
+    exit(1)
+
+if argv[1] == "A":
+    os.system("ssh -i g.rsa theuser@192.168.109.154")
+elif argv[1] == "B":
+    os.system("ssh -i g.rsa theuser@192.168.109.154 -t ssh -i .ssh/privBC.rsa theuser@192.168.88.101")
+elif argv[1] == "C":
+    os.system("ssh -i g.rsa theuser@192.168.109.154 -t ssh -i .ssh/privBC.rsa theuser@192.168.88.102")
