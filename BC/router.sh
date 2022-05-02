a9ca683e7e6bcc26cc3ef96c89463730716e6d90
Xavier Pisco <x.pisco@gmail.com>
2022-05-02 16:49:36 +0100

    Router only enables port 80

--- BC/router.sh
+++ BC/router.sh
@@ -6,3 +6,6 @@ netubuntu
 
 sudo docker network connect server_net \
 router --ip 10.0.2.254
+
+sudo docker exec router /bin/bash -c \
+'iptables -t filter -A FORWARD -p tcp -- dport 80 ! -s 10.0.1.253 -j DROP'
